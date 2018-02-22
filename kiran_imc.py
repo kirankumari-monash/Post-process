3f27363555a3a95eb4fc97bdaf47b2748c6876fb
kiran <kirankri27@gmail.com>
2018-02-22 18:47:18 +1100

    22/02/2018

--- kiran_imc.py
+++ kiran_imc.py
@@ -5,10 +5,9 @@ from scipy.io import netcdf
 #from numpy.linalg import inv
 iteration = 0
 ite_done = 0
-#iteration_read = 1  # 1 for reading the number of iteration from the file
-iteration_read = 0
-#phi_new = 1 # 1 for reading the cp ref and calculating the new phi
-phi_new = 0
+iteration_read = 1  # 1 for reading the number of iteration from the file
+#iteration_read = 0
+phi_new = 1 # 1 for reading the cp ref and calculating the new phi
 cp_ens =0
 b_obs_ens =0
 cp_ens_1d = 0
@@ -18,14 +17,16 @@ r_error = 0
 rg_cum = 0
 rg_error = 0
 b_obs_ens_1d = 0
+b_obs_error = 0
 covar_1_ens = 0
+covar_1_error = 0
 cpr =1.82
 al = 1.5306333
 be = 1.213115524
-epsilonr =0.01
+epsilonr =0.1
 k_bT = 1 
-slicer =0 #Initial samples to be discarded
-trajt = 5
+slicer =5 #Initial samples to be discarded
+trajt = 50
 for traj in range(trajt):
   f = netcdf.NetCDFFile('net_%s.nc' %(traj+101),'r',mmap=False)
 #########################################################
@@ -118,7 +119,6 @@ for traj in range(trajt):
 ########### 1D
   argr=0
   b_obs_1d = np.zeros((arg,Nsample))
-  #print 'cpzero',cp_1d
   for nu in range (NBeads):
     for mu in range (nu):
       b_obs_1d[argr,:] = b_obs[nu,mu,:]
@@ -137,11 +137,14 @@ for traj in range(trajt):
     A[:,0] = cp_1d[:,Ns]
     AT = np.transpose(A)
     covar_1[:,:] = np.matmul(B,AT)
+    print 'cov', covar_1
     covar_1time = covar_1time + covar_1
-#    print 'covar_1', covar_1
+  print 'cp', cp_1d
+  print 'b_obs', b_obs_1d
+  print 'covar_1', covar_1time
+  
 #    print 'sample completed', Ns    
-  print 'Completed traj', traj
-  covar_1time = covar_1time/(Nsample)
+#  print 'Completed traj', traj
 ################# global averages
   r_ens = np.sum(r_sqs[:],axis=0)
   r_ens = r_ens/(Nsample)
@@ -163,8 +166,23 @@ for traj in range(trajt):
 #  print 'cp_ens_sq', cp_ens_sq
   cp_error = cp_error + cp_ens_sq
   
-  b_obs_ens_1d = b_obs_ens_1d + b_obs_1d
+  b_obs_ens = np.sum(b_obs_1d[:],axis=1)
+  b_obs_ens = b_obs_ens/(Nsample)
+  b_obs_ens_1d = b_obs_ens_1d + b_obs_ens
+  b_obs_sq = np.square(b_obs_ens)
+  b_obs_error = b_obs_error + b_obs_sq
+
+  covar_1time = covar_1time/(Nsample)
+  print 'covar_1_time', covar_1time
+  covar_1sq = np.square(covar_1time)
   covar_1_ens = covar_1_ens + covar_1time
+  print 'covar_1_ens', covar_1_ens 
+#  covar_1_ens_sq = np.square(covar_1_ens)
+  print 'sq', covar_1_ens
+  covar_1_error = covar_1_error + covar_1sq
+  print 'Completed traj', traj, covar_1_error
+  print '-----------------------------------'
+print '##################################################################'
 #  print cp
 ##########################################################
 ######### Ensemble averaging 
@@ -195,16 +213,30 @@ cp_error = cp_error/(trajt)
 cp_error = np.subtract(cp_error,cp_ens_1d_sq)
 cp_error = cp_error/(trajt-1)
 cp_error = np.sqrt(cp_error)
-#print 'cp_error', cp_error
+print 'cp', cp_ens_1d
+print 'cp_error', cp_error
 b_obs_ens_1d = b_obs_ens_1d/(trajt)
+b_obs_ens_1d_sq = np.square(b_obs_ens_1d)
+b_obs_error = b_obs_error/(trajt)
+b_obs_error = np.subtract(b_obs_error,b_obs_ens_1d_sq)
+b_obs_error = b_obs_error/(trajt-1)
+b_obs_error = np.sqrt(b_obs_error)
+print 'b', b_obs_ens_1d
+print 'b_error', b_obs_error
 covar_1_ens = covar_1_ens/(trajt)
+print 'covar_1ens', covar_1_ens
+covar_1_ens_sq = np.square(covar_1_ens)
+print 'covar_ens_sq', covar_1_ens_sq
+covar_1_error = covar_1_error/(trajt)
+print 'sq', covar_1_error, trajt
+covar_1_error = np.subtract(covar_1_error,covar_1_ens_sq)
+print 'pata nai', covar_1_error
+covar_1_error = covar_1_error/(trajt-1)
+print 'co error', covar_1_error
+covar_1_error = np.sqrt(covar_1_error)
+print 'covar1_ens', covar_1_ens
+print 'covar1_error', covar_1_error
 #########################################################
-#######  Time Ensemble average  
-#########################################################
-
-b_obs_time = np.sum(b_obs_ens_1d[:],axis=1)
-b_obs_time = b_obs_time/(Nsample)
-
 ################## cp to 2d
 argr=0
 cp_check = np.zeros((NBeads,NBeads))
@@ -220,11 +252,30 @@ for nu in range (NBeads):
 #####################################################
 ##### covariave 2nd part
 covar_2 = np.zeros((arg,arg))
+covar_2_error = np.zeros((arg,arg))
+covar_2_mag = np.zeros((arg,arg))
 for nu in range (arg):
   for mu in range (arg):
-    covar_2[nu,mu] = (b_obs_time[nu]) *( cp_ens_1d[mu])
-#print 'co2', covar_2
+    covar_2[nu,mu] = (b_obs_ens_1d[nu]) *( cp_ens_1d[mu])
+    print 'covar2', covar_2[nu,mu], cp_ens_1d[mu], b_obs_ens_1d[nu]
+    covar_2_error1 = b_obs_error[nu]/b_obs_ens_1d[nu]
+    covar_2_error1 = np.square(covar_2_error1)
+    covar_2_error2 = cp_error[mu]/cp_ens_1d[mu]
+    covar_2_error2 = np.square(covar_2_error2)
+    covar_2_error[nu,mu] = covar_2_error1 + covar_2_error2
+    covar_2_error[nu,mu] = np.sqrt(covar_2_error[nu,mu])
+covar_2_mag = np.square(covar_2)
+covar_2_mag = np.sqrt(covar_2_mag)
+covar_2_error = covar_2_mag * covar_2_error     
+print 'co2', covar_2
+print 'covar_2 error', covar_2_error
 covar_final = k_bT*np.subtract(covar_1_ens[:],covar_2[:])
+covar_1_error = np.square(covar_1_error)
+covar_2_error = np.square(covar_2_error)
+covar_error = k_bT*(covar_1_error + covar_2_error)
+covar_error = np.sqrt(covar_error)
+print 'covar_final', covar_final
+print 'covar_error', covar_error
 #print 'final', covar_final
 
 #### Inverse of covariance matrix
@@ -270,6 +321,13 @@ fcpi.write("\n")
 fcpi.write("\n")
 fcpi.close()
 
+fcpi = open("contprob_itera_1d.txt","a+")
+fcpi.write("Iteration number %i\n"%iteration)
+for argr in range (arg):
+  fcpi.write("%i\t%f\t%f\n"%(argr+1,cp_ens_1d[argr],cp_error[argr]))
+fcpi.close()
+
+
 fcp = open("contprob_new.txt","w")
 fcp.write("Contact Probability")
 fcp.write("\n")
@@ -294,24 +352,33 @@ fcp.close()
 ########        Reference Contact probability    ############
 if phi_new == 1:
   cp_ref = np.zeros((NBeads,NBeads))
+  cp_ref_error  = np.zeros((NBeads,NBeads))
   f = open("cp_ref.txt","r")
   lines =  f.readlines()
-  nu=0
+  lineno = 1
+  cp_lineno = 0
+  cp_err_lineno = 0
   for line in lines:
     line = line.strip()
     data= line.split()
-#    ptin1 = float(data[0])
-#    ptin2 = float(data[1])
-#    ptin3 = float(data[2])
-    for mu in range (NBeads):
-      cp_ref[nu,mu] = float(data[mu])
-    nu=nu+1   
+    if lineno >1 and lineno < NBeads +2:
+      for mu in range (NBeads):
+        cp_ref[cp_lineno,mu] = float(data[mu])
+      cp_lineno+=1
+    if lineno > NBeads +3 and lineno < 2*NBeads + 4:
+      for mu in range (NBeads):
+        cp_ref_error[cp_err_lineno,mu] = float(data[mu])
+      cp_err_lineno +=1 
+    lineno+=1  
   print 'cpref', cp_ref
+  print 'cp_ref_err', cp_ref_error
   argr = 0
   cp_ref_1d = np.zeros((arg))
+  cp_ref_error_1d = np.zeros((arg))
   for nu in range (NBeads):
     for mu in range (nu):
       cp_ref_1d[argr] = cp_ref[nu,mu]
+      cp_ref_error_1d[argr] = cp_ref_error[nu,mu]
       argr = argr +1
 #  print 'old', cp_ref_1d
 ################################################
@@ -354,6 +421,17 @@ if phi_new == 1:
       phi_old[nu,mu] = float(data[mu])
     nu=nu+1
 #  print 'phi_old', phi_old
+
+
+#  f_ite = open("phi_ite.txt","a+")
+#  f_ite.write("Iteration number = %i\n"%iteration)
+#  for nu in range (NBeads):
+#    for mu in range (NBeads):
+#      f_ite.write("%f\t" %(phi_old[nu,mu]))
+#    f_ite.write("\n")
+#  f_ite.write("\n")
+#  f_ite.close()
+
 #########  converting phi to 1d
   argr = 0
   phi_old_1d = np.zeros((arg))
@@ -366,6 +444,11 @@ if phi_new == 1:
 #########
   delta_cp =np.subtract(cp_ref_1d[:],cp_ens_1d[:])
   print 'cp_new', cp_ens_1d
+  delta_cp_error1 = np.square(cp_error)
+  delta_cp_error2 = np.square(cp_ref_error_1d)
+  delta_cp_error = delta_cp_error1 + delta_cp_error2
+  delta_cp_error = np.sqrt(delta_cp_error)
+  delta_cp_error = epsilonr* delta_cp_error
 #  print 'delta cp', delta_cp
   delta_cp = epsilonr*delta_cp
 #  print 'cp_ref-cp', delta_cp
@@ -373,8 +456,15 @@ if phi_new == 1:
 #  print 'phi_dummy',phi_dummy
 #  print 'covar',covar_final
   phi_new_1d = np.linalg.solve(covar_final,delta_cp)
+  print 'phi_new',phi_new_1d
+  phi_error1 =  np.matmul(covar_error,phi_new_1d)
+  print 'phi_err1', phi_error1
 #  print 'phi_without', phi_new_1d
+  phi_error1 = np.subtract(delta_cp_error,phi_error1)
+  phi_error_1d = np.linalg.solve(covar_final,phi_error1)
+  print 'phi_error', phi_error_1d
   phi_new_1d = phi_new_1d + phi_old_1d
+  print 'phi', phi_new_1d
 #  phi_new_1d = np.dot(covar_inv,delta_cp)
 #  phi_new_1d = epsilonr*phi_new_1d
 #  phi_new_1d = phi_old_1d + phi_new_1d
@@ -384,11 +474,37 @@ if phi_new == 1:
 #converting phi back to matrix
   argr=0
   phi_new = np.zeros((NBeads,NBeads))
+  phi_error = np.zeros((NBeads,NBeads))
   for nu in range (NBeads):
     for mu in range (nu):
       phi_new[nu,mu] = phi_new_1d[argr]
       phi_new[mu,nu] = phi_new[nu,mu]
+      phi_error[nu,mu] = phi_error_1d[argr]
+      phi_error[mu,nu] = phi_error[nu,mu]
       argr = argr +1
+  f_ite = open("phi_ite.txt","a+")
+  f_ite.write("Iteration number = %i\n"%iteration)
+  for nu in range (NBeads):
+    for mu in range (NBeads):
+      f_ite.write("%f\t" %(phi_new[nu,mu]))
+    f_ite.write("\n")
+  f_ite.write("\n")
+  f_ite.write("Error in phi\n")
+  for nu in range (NBeads):
+    for mu in range (NBeads):
+      f_ite.write("%f\t" %(phi_error[nu,mu]))
+    f_ite.write("\n")
+  f_ite.write("\n")
+  f_ite.close()
+
+  f_1d = open("phi_ite_1d.txt","a+")
+  for argr in range(arg):
+    f_1d.write("Iteration number = %i\n"%iteration)
+    f_1d.write("%i\t%f\t%f\n"%(argr+1,phi_new_1d[argr],phi_error_1d[argr]))
+  f_1d.write("\n")
+  f_1d.close()
+
+
   print 'phi', phi_new
   file = open("phi.txt","w")
   for nu in range (NBeads):
@@ -400,19 +516,19 @@ if phi_new == 1:
       file.write("%f\t" %(phi_new[nu,mu]))
     file.write("\n")
   file.close()
-  iteration = iteration +1 
-  f_ite = open("phi_ite.txt","a+")
-  f_ite.write("Iteration number = %i\n"%iteration)
-  for nu in range (NBeads):
-    for mu in range (NBeads):
+#  iteration = iteration +1 
+#  f_ite = open("phi_ite.txt","a+")
+#  f_ite.write("Iteration number = %i\n"%iteration)
+#  for nu in range (NBeads):
+#    for mu in range (NBeads):
 #      if phi_new[nu,mu] <0:
 #        phi_new[nu,mu] = 0
 #      if phi_new[nu,mu] >1:
 #        phi_new[nu,mu] = 1
-      f_ite.write("%f\t" %(phi_new[nu,mu]))
-    f_ite.write("\n")
-  f_ite.write("\n")
-  f_ite.close()
+#      f_ite.write("%f\t" %(phi_new[nu,mu]))
+#    f_ite.write("\n")
+#  f_ite.write("\n")
+#  f_ite.close()
 
 ##############################################################
 ############           Trial for writting   
